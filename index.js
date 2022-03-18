b049832baf5940da4f41aaa3f12903dfe6f9eee2
Ajdean37 <ajdean37@gmail.com>
2022-03-18 16:55:26 -0500

    changes

--- index.js
+++ index.js
@@ -12,12 +12,58 @@ function handleReddit( e ) {
     console.log("Reddit Clicked");
 }
 
-function handleId1( e ) {
-    console.log("link Clicked");
+async function handleGetData( e ) {
+    console.log("refreshed clicked");
+
+    let response = await fetch('https://api.spacexdata.com/v4/launches/');
+    console.log(response);
+    let data = await response.json();
+    console.log(data);
+    for (const launch of data) {
+        // console.log(launch.links.patch.small);
+        // console.log(launch.name);
+        // console.log(launch.upcoming);
+        let date = new Date(launch.date_utc);
+        // console.log (date.toLocalDateString() );
+        // console.log(launch.details);
+        // console.log(launch.id);
+        // console.log(launch.links.article);
+
+        let tableBody = document.querySelector('#table-body-target');
+        let tr = document.createElement('tr')
+
+        let innerHTML = `<td scope="row"><img src="${launch.links.patch.small}" alt=falconSat height="25px"></td>
+        <td>${launch.name}</td>
+        <td>${launch.upcoming}</td>
+        <td>${date.toLocaleDateString()}</td>
+        <td>${launch.details}</td>
+        <td>${launch.flight_number}</td>
+        <td><a href="${launch.links.article}"><i class="fa-solid fa-link"></i></a></td>`;
+
+        tr.innerHTML = innerHTML;
+        tableBody.appendChild(tr);
+
+    }
+
 }
 
+function testAppend (e) {
+    // console.log(e);
+    let tableBody = document.querySelector('#table-body-target');
+    // console.log(tableBody);
+    let tr = document.createElement('tr')
+    console.log(tr);
+    tr.innerHTML =  `<td scope="row"><img src="https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png" alt=falconSat height="25px"></td>
+    <td>FalconSat</td>
+    <td>Merlin A</td>
+    <td>03/24/2006</td>
+    <td>Engine failure at 33 seconds and loss of vehicle</td>
+    <td>1</td>
+    <td id="id1"><i class="fa-solid fa-link"></i><a href="https://www.space.com/2196-spacex-inaugural-falcon-1-rocket-lost-launch.html"></a></td>`;
 
+    tableBody.appendChild(tr);
 
+}
 
 
 
@@ -36,8 +82,11 @@ function readyDOM() {
     let checkboxReddit = document.querySelector('#checkboxReddit');
     checkboxReddit.addEventListener('click', handleReddit);
 
-    let id1 = document.querySelector('#id1');
-    id1.addEventListener('click', handleId1);
+    let testAppendBtn = document.querySelector('#testAppendBtn');
+    testAppendBtn.addEventListener('click', testAppend)
+
+    let refreshBtn = document.querySelector('#refresh');
+    refreshBtn.addEventListener('click', handleGetData)
 }
 
 
